7cee760bc71e6c56709d63bc6f39cd231aaa17be
Oguz Pastirmaci <oguz.pastirmaci@oracle.com>
2026-03-25 15:01:38 -0700

    test: expand CoreProvisioning assertions for pools, LBs, endpoints, bastion, operator

--- test/core_provisioning_test.go
+++ test/core_provisioning_test.go
@@ -1,6 +1,7 @@
 package test
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/gruntwork-io/terratest/modules/terraform"
@@ -52,6 +53,47 @@ func TestCoreProvisioning(t *testing.T) {
 	workerNsgID := terraform.Output(t, options, "worker_nsg_id")
 	require.True(t, isValidOCID(workerNsgID), "worker_nsg_id should be a valid OCID: %s", workerNsgID)
 
-	// Validate endpoint is present (not an OCID)
-	require.NotEmpty(t, terraform.Output(t, options, "cluster_private_endpoint"))
+	// Ops worker pool is always created
+	workerOpsPoolID := terraform.Output(t, options, "worker_ops_pool_id")
+	require.True(t, isValidOCID(workerOpsPoolID), "worker_ops_pool_id should be a valid OCID: %s", workerOpsPoolID)
+
+	// Internal LB subnet/NSG are always created
+	intLbSubnetID := terraform.Output(t, options, "int_lb_subnet_id")
+	require.True(t, isValidOCID(intLbSubnetID), "int_lb_subnet_id should be a valid OCID: %s", intLbSubnetID)
+
+	intLbNsgID := terraform.Output(t, options, "int_lb_nsg_id")
+	require.True(t, isValidOCID(intLbNsgID), "int_lb_nsg_id should be a valid OCID: %s", intLbNsgID)
+
+	// Cluster private endpoint is always present and must be HTTPS
+	clusterPrivateEndpoint := terraform.Output(t, options, "cluster_private_endpoint")
+	require.NotEmpty(t, clusterPrivateEndpoint)
+	require.True(t, strings.HasPrefix(clusterPrivateEndpoint, "https://"), "cluster_private_endpoint should start with https://: %s", clusterPrivateEndpoint)
+
+	// Public endpoint only present when control plane is public
+	clusterPublicEndpoint := terraform.Output(t, options, "cluster_public_endpoint")
+	if clusterPublicEndpoint != "" {
+		require.True(t, strings.HasPrefix(clusterPublicEndpoint, "https://"), "cluster_public_endpoint should start with https://: %s", clusterPublicEndpoint)
+	}
+
+	// Public LB subnet/NSG only present when public subnets are enabled
+	pubLbSubnetID := terraform.Output(t, options, "pub_lb_subnet_id")
+	if pubLbSubnetID != "" {
+		require.True(t, isValidOCID(pubLbSubnetID), "pub_lb_subnet_id should be a valid OCID: %s", pubLbSubnetID)
+
+		pubLbNsgID := terraform.Output(t, options, "pub_lb_nsg_id")
+		require.True(t, isValidOCID(pubLbNsgID), "pub_lb_nsg_id should be a valid OCID: %s", pubLbNsgID)
+	}
+
+	// Bastion only present when create_bastion = true
+	bastionID := terraform.Output(t, options, "bastion_id")
+	if bastionID != "" {
+		require.True(t, isValidOCID(bastionID), "bastion_id should be a valid OCID: %s", bastionID)
+		require.NotEmpty(t, terraform.Output(t, options, "bastion_public_ip"), "bastion_public_ip should not be empty when bastion is created")
+	}
+
+	// Operator only present when create_operator = true
+	operatorID := terraform.Output(t, options, "operator_id")
+	if operatorID != "" {
+		require.True(t, isValidOCID(operatorID), "operator_id should be a valid OCID: %s", operatorID)
+	}
 }
