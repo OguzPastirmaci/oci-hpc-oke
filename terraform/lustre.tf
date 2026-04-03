024b5004fe99c6100e0976af3fc3b210048dfb36
Oguz Pastirmaci <oguz.pastirmaci@oracle.com>
2026-04-03 15:07:01 -0700

    Increase Lustre prerequisite sleep to 60s

--- terraform/lustre.tf
+++ terraform/lustre.tf
@@ -272,7 +272,7 @@ resource "time_sleep" "wait_for_lustre_prerequisites" {
     oci_identity_policy.lustre_service_network,
   ]
 
-  create_duration = "30s"
+  create_duration = "60s"
 }
 
 resource "oci_lustre_file_storage_lustre_file_system" "lustre" {
